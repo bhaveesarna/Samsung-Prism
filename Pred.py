b080cc53945389390e7c9a8dbc9e8f0abf985c71
bhaveesarna <bhaveesarna@gmail.com>
2023-02-27 15:25:27 +0530

    adds switch cases and loops

--- Pred.py
+++ Pred.py
@@ -2,6 +2,12 @@ from Test_Model import *
 from Parse import *
 import random
 import string
+import re
+
+TOKENS = [':','\(','\)','/','{','}','<','>','==','<=','>=','\s']
+TOKEN_SPLITTER = "|".join(TOKENS)
+
+
 
 
 
@@ -20,16 +26,35 @@ def predictCaseVals(func:function):
         #             cases_to_add.append(("Content","Autotest",func,[[int(i)-1,int(i),int(i)+1]*len(func.args["int"])]))
         # else:
         conditions = []
+        loops = []
+        switches = []
         lines = func.content.split(';')
         for idx in range(len(lines)):
             if 'if' in lines[idx]:
                 conditions.append(lines[idx])
+            if 'for' in lines[idx]:
+                loops.append(lines[idx])
+            if 'while' in lines[idx]:
+                loops.append(lines[idx])
+            if 'case' in lines[idx]:
+                switches.append(lines[idx])
         for i in conditions:
-            for j in i.split():
-                if j.isdigit() and (i.index('if') < i.index(j)) and checkDuplicate(cases_to_add,func,j):
+            for j in re.split(TOKEN_SPLITTER,i):
+                if j and j.isdigit() and (i.index('if') < i.index(j)):
                     cases_to_add.append(("BVA","Autotest",func,int(j)-1))
                     cases_to_add.append(("BVA","Autotest",func,int(j)))
                     cases_to_add.append(("BVA","Autotest",func,int(j)+1))
+        for i in loops:
+            for j in re.split(TOKEN_SPLITTER,i):
+                if j and j.isdigit() and checkDuplicate(cases_to_add,func,j):
+                    cases_to_add.append(("loop","Autotest",func,int(j)-1))
+                    cases_to_add.append(("loop","Autotest",func,int(j)))
+                    cases_to_add.append(("loop","Autotest",func,int(j)+1))
+        
+        for i in switches:
+            for j in re.split(TOKEN_SPLITTER,i):
+                if j and j.isdigit() and checkDuplicate(cases_to_add,func,j):
+                    cases_to_add.append(("switchcase","Autotest",func,int(j)))
 
         cases_to_add.append(("Positive","Autotest",func,random.randint(0,100)))
         cases_to_add.append(("Negative","Autotest",func,random.randint(-1*100,0)))
