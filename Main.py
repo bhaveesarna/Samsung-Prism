e5ab161924dc75c6b2f1874cca44cede0c487ca4
bhaveesarna <bhaveesarna@gmail.com>
2023-02-27 21:07:38 +0530

    removes unnecessary tests in main

--- Main.py
+++ Main.py
@@ -26,20 +26,6 @@ for pf_name in pf_names:
     time_limit = input("Time limit for test cases: ")
     gen.write_Quicktest(time_limit, hf_name)
 
-
-    t = Test('Positive','FactorialTest')
-    t.add_assertion("EXPECT_EQ",['120','Factorial(5)'])
-    t.add_assertion("EXPECT_EQ",['24','Factorial(4)'])
-    t.add_assertion("EXPECT_EQ",['6','Factorial(3)'])
-    t.add_assertion("EXPECT_EQ",['40320','Factorial(8)'])
-    gen.write_test(t)
-
-
-    t2 = Test('PrimeTest1','IsPrimeTestSuite')
-    t2.add_assertion("EXPECT_EQ",['false','IsPrime(10)'])
-    t2.add_assertion("EXPECT_EQ",['true','IsPrime(3)'])
-    gen.write_test(t2)
-
     funcs = gen.get_functions(pf_name)
     cases = addPredictedCases(funcs)
     for case in cases:
