e995db63f62f6328dafd2bf9d52e79fea1b30fc7
bhavvyasarna <bhavvya.sarna2020@vitstudent.ac.in>
2023-04-02 23:41:43 +0530

    adds char support

--- Multiple_args.py
+++ Multiple_args.py
@@ -58,14 +58,87 @@ class Multiple_args:
         ip_string = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase, k=random.randint(5,10)))
         strs.append(ip_string)
         return strs
+
+    def char_case(self,func):
+        chars = []
+        conditions = []
+        lines = func.content.split(';')
+        for idx in range(len(lines)):
+            if 'if' in lines[idx]:
+                conditions.append(lines[idx])
+        for i in conditions:
+            for j in re.split(TOKEN_SPLITTER,i):
+                if j and (i.index('if') < i.index(j)) and re.match("'.'",j) and not j.isdigit():
+                    j = j.strip("'")
+                    chars.append(j)
+                    chars.append(chr(ord(j)-1))
+                    chars.append(chr(ord(j)+1))
+        chars.append(chr(ord(random.randint(97,122))))
+        chars = list(set(chars))
+        return chars
+        
+        
+    def float_case(self,func):
+        def is_float(string):
+            try:
+                float(string)
+                return True
+            except ValueError:
+                return False
+        
+        floats = []
+        conditions = []
+        loops = []
+        switches = []
+        lines = func.content.split(';')
+        for idx in range(len(lines)):
+            if 'if' in lines[idx]:
+                conditions.append(lines[idx])
+            if 'for' in lines[idx]:
+                loops.append(lines[idx])
+            if 'while' in lines[idx]:
+                loops.append(lines[idx])
+            if 'case' in lines[idx]:
+                switches.append(lines[idx])
+        for i in conditions:
+            for j in re.split(TOKEN_SPLITTER,i):
+                if j and is_float(j) and (i.index('if') < i.index(j)):
+                    floats.append(float(j)-random.random())
+                    floats.append(float(j))
+                    floats.append(float(j)+random.random())
+        for i in loops:
+            for j in re.split(TOKEN_SPLITTER,i):
+                if j and is_float(j):
+                    floats.append(float(j)-random.random())
+                    floats.append(float(j))
+                    floats.append(float(j)+random.random())
+        
+        for i in switches:
+            for j in re.split(TOKEN_SPLITTER,i):
+                if j and is_float(j):
+                    floats.append(float(j))
+
+        floats.append(random.random()*100)
+        floats.append(random.random()*-100)
+        floats.append(0.0)
+        floats = list(set(floats))
+        return floats
     
     def cases(self):
         ip_lists = []
         for arg in self.func.args_list:
-            if arg[0] == "int" or arg[0] == "float":
+            if arg[0] == "int":
                 ip_lists.append(self.num_case(self.func))
-            else:
+            elif arg[0].lower() == "string":
                 ip_lists.append(self.string_case())
+            elif arg[0] == "char":
+                ip_lists.append(self.char_case(self.func))
+            elif arg[0].lower() == "float":
+                ip_lists.append(self.float_case(self.func))
+            else:
+                ip_lists.append([" "])
+
+
         product = []       
         for item in itertools.product(*ip_lists):
 	        product.append(item)
--- Pred.py
+++ Pred.py
@@ -80,6 +80,74 @@ def predictCaseVals(func:function):
             ip_string = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase, k=random.randint(5,10)))
             cases_to_add.append(("Mixed","Autotest",func,ip_string))
 
+        elif func.args_list[0][0] == "char":
+            chars = []
+            conditions = []
+            lines = func.content.split(';')
+            for idx in range(len(lines)):
+                if 'if' in lines[idx]:
+                    conditions.append(lines[idx])
+            for i in conditions:
+                for j in re.split(TOKEN_SPLITTER,i):
+                    if j and (i.index('if') < i.index(j)) and len(j.strip("'")) == 1 and not j.isdigit():
+                        j = j.strip("'")
+                        cases_to_add.append(("BVA_char","Autotest",func,j))
+                        cases_to_add.append(("BVA_more_char","Autotest",func,chr(ord(j)+1)))   
+                        cases_to_add.append(("BVA_less_char","Autotest",func,chr(ord(j)-1)))  
+        
+            cases_to_add.append(("Random_char","Autotest",func,chr(ord(random.randint(97,122)))))  
+             
+                         
+        elif func.args_list[0][0] == "float":
+            def is_float(string):
+                try:
+                    float(string)
+                    return True
+                except ValueError:
+                    return False
+            
+            conditions = []
+            loops = []
+            switches = []
+            lines = func.content.split(';')
+            for idx in range(len(lines)):
+                if 'if' in lines[idx]:
+                    conditions.append(lines[idx])
+                if 'for' in lines[idx]:
+                    loops.append(lines[idx])
+                if 'while' in lines[idx]:
+                    loops.append(lines[idx])
+                if 'case' in lines[idx]:
+                    switches.append(lines[idx])
+            for i in conditions:
+                for j in re.split(TOKEN_SPLITTER,i):
+                    if j and is_float(j) and (i.index('if') < i.index(j)):
+                        cases_to_add.append(("BVA","Autotest",func,float(j)-random.random())) 
+                        cases_to_add.append(("BVA","Autotest",func,float(j)))
+                        cases_to_add.append(("BVA","Autotest",func,float(j)+random.random()))
+            for i in loops:
+                for j in re.split(TOKEN_SPLITTER,i):
+                    if j and j.isdigit():
+                        if int(j)-1 not in nums_done:
+                            cases_to_add.append(("loop","Autotest",func,float(j)-random.random()))
+                            nums_done.add(int(j)-1)
+                        if int(j) not in nums_done:
+                            cases_to_add.append(("loop","Autotest",func,float(j)))
+                            nums_done.add(int(j))
+                        if int(j)+1 not in nums_done:
+                            cases_to_add.append(("loop","Autotest",func,float(j)+random.random()))
+                            nums_done.add(int(j)+1)
+            
+            for i in switches:
+                for j in re.split(TOKEN_SPLITTER,i):
+                    if j and j.isdigit():
+                        cases_to_add.append(("switchcase","Autotest",func,float(j)))
+
+            cases_to_add.append(("Positive","Autotest",func,random.random()*100))
+            cases_to_add.append(("Negative","Autotest",func,random.random()*-100))
+            if 0 not in nums_done:
+                cases_to_add.append(("Zero","Autotest",func,0.0))
+
     else:
         mc = Multiple_args(func)
         cases_to_add = mc.cases()
